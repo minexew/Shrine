86a119918e771fc380485155bb1d334e00412a2c
minexew <minexew@gmail.com>
2017-02-05 20:15:36 +0100

    Http: add User-Agent header; fix patch script

--- Shrine/apply-patches.sh
+++ Shrine/apply-patches.sh
@@ -9,8 +9,8 @@ rm -rf $PATCHED_DIR
 cp -rp $PURE_DIR $PATCHED_DIR
 
 patch $FLAGS -i Branding/ADefine.diff
-patch $FLAGS -i Branding/MakeAdam.diff
 patch $FLAGS -i Branding/Start.diff
 patch $FLAGS -i Distro/Comm.diff
 patch $FLAGS -i Distro/DoDistro.diff
+patch $FLAGS -i Distro/MakeAdam.diff
 patch $FLAGS -i Distro/Once.diff
