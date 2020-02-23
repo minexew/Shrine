05443896c0cc215c7178303764e0b0befff17d59
minexew <minexew@gmail.com>
2020-02-23 18:07:35 +0100

    VKernel: Progress to the point of successfully loading Compiler

--- VKernel/apply-patches.sh
+++ VKernel/apply-patches.sh
@@ -8,9 +8,19 @@ FLAGS="-u -p1"
 rm -rf $PATCHED_DIR
 cp -rp $PURE_DIR $PATCHED_DIR
 
+patch $FLAGS -i BlkPool.diff
 patch $FLAGS -i Display.diff
+patch $FLAGS -i DskAddDev.diff
+patch $FLAGS -i DskDrv.diff
+patch $FLAGS -i DskFile.diff
+patch $FLAGS -i DskStrB.diff
 patch $FLAGS -i KDate.diff
+patch $FLAGS -i KDbg.diff
+patch $FLAGS -i KDefine.diff
 patch $FLAGS -i KMisc.diff
 patch $FLAGS -i KUtils.diff
-patch $FLAGS -i BlkPool.diff
+patch $FLAGS -i KernelA.diff
+patch $FLAGS -i MakeBlkDev.diff
 patch $FLAGS -i MemPhysical.diff
+patch $FLAGS -i MultiProc.diff
+patch $FLAGS -i Sched.diff
--- VKernel/make-patches.sh
+++ VKernel/make-patches.sh
@@ -5,9 +5,19 @@ PATCHED_DIR=Patched
 
 FLAGS="--no-index"
 
+git diff $FLAGS "$PURE_DIR/Kernel/BlkDev/DskAddDev.HC" "$PATCHED_DIR/Kernel/BlkDev/DskAddDev.HC" >DskAddDev.diff
+git diff $FLAGS "$PURE_DIR/Kernel/BlkDev/DskDrv.HC" "$PATCHED_DIR/Kernel/BlkDev/DskDrv.HC" >DskDrv.diff
+git diff $FLAGS "$PURE_DIR/Kernel/BlkDev/DskFile.HC" "$PATCHED_DIR/Kernel/BlkDev/DskFile.HC" >DskFile.diff
+git diff $FLAGS "$PURE_DIR/Kernel/BlkDev/DskStrB.HC" "$PATCHED_DIR/Kernel/BlkDev/DskStrB.HC" >DskStrB.diff
+git diff $FLAGS "$PURE_DIR/Kernel/BlkDev/MakeBlkDev.HC" "$PATCHED_DIR/Kernel/BlkDev/MakeBlkDev.HC" >MakeBlkDev.diff
 git diff $FLAGS "$PURE_DIR/Kernel/Display.HC" "$PATCHED_DIR/Kernel/Display.HC" >Display.diff
 git diff $FLAGS "$PURE_DIR/Kernel/KDate.HC" "$PATCHED_DIR/Kernel/KDate.HC" >KDate.diff
+git diff $FLAGS "$PURE_DIR/Kernel/KDbg.HC" "$PATCHED_DIR/Kernel/KDbg.HC" >KDbg.diff
+git diff $FLAGS "$PURE_DIR/Kernel/KDefine.HC" "$PATCHED_DIR/Kernel/KDefine.HC" >KDefine.diff
 git diff $FLAGS "$PURE_DIR/Kernel/KMisc.HC" "$PATCHED_DIR/Kernel/KMisc.HC" >KMisc.diff
 git diff $FLAGS "$PURE_DIR/Kernel/KUtils.HC" "$PATCHED_DIR/Kernel/KUtils.HC" >KUtils.diff
+git diff $FLAGS "$PURE_DIR/Kernel/KernelA.HH" "$PATCHED_DIR/Kernel/KernelA.HH" >KernelA.diff
 git diff $FLAGS "$PURE_DIR/Kernel/Mem/BlkPool.HC" "$PATCHED_DIR/Kernel/Mem/BlkPool.HC" >BlkPool.diff
 git diff $FLAGS "$PURE_DIR/Kernel/Mem/MemPhysical.HC" "$PATCHED_DIR/Kernel/Mem/MemPhysical.HC" >MemPhysical.diff
+git diff $FLAGS "$PURE_DIR/Kernel/MultiProc.HC" "$PATCHED_DIR/Kernel/MultiProc.HC" >MultiProc.diff
+git diff $FLAGS "$PURE_DIR/Kernel/Sched.HC" "$PATCHED_DIR/Kernel/Sched.HC" >Sched.diff
